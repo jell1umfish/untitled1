f22c9bd6cf2f21467b08dc3624468a6c78991b47
Lenovo <jell1umfish@gmail.com>
2023-05-01 22:32:15 +0600

    constructor

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,5 +1,10 @@
 import java.util.LinkedList;
 
 public class MyLinkedListStack<T> {
+    private LinkedList<T> list;
+
+    public MyLinkedListStack() {
+        list = new LinkedList<>();
+    }
 
 }
