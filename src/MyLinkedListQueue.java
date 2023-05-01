761edbdb55b086577ef795fd3ae581c7a25bbf53
Lenovo <jell1umfish@gmail.com>
2023-05-01 23:14:40 +0600

    constructor

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
 public class MyLinkedListQueue<T> {
+    private ArrayList<T> list;
+
+    public MyLinkedListQueue() {
+        list = new ArrayList<>();
+    }
 
 }
