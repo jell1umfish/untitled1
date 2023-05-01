81598293ab77060398e5d96c5e5798e1cd63c954
Lenovo <jell1umfish@gmail.com>
2023-05-01 22:41:42 +0600

    pop push peek

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -6,5 +6,24 @@ public class MyLinkedListStack<T> {
     public MyLinkedListStack() {
         list = new LinkedList<>();
     }
+    public void push(T data) {
+        list.add(data);
+    }
 
+    public T pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack is empty");
+        }
+        return list.remove(list.size() - 1);
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack is empty");
+        }
+        return list.get(list.size() - 1);
+    }
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
 }
