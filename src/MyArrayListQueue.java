6ce6b6057eb03b423b6a18215efd69913c7e2246
Lenovo <jell1umfish@gmail.com>
2023-05-01 23:20:32 +0600

    peek

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -19,4 +19,10 @@ public class MyArrayListQueue<T> {
         }
         return list.remove(0);
     }
+    public T peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return list.get(0);
+    }
 }
